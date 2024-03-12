040b0f4b74590c846b4cf816bef30fad4ce1b9e0
owaite <olivia.jm.waite@gmail.com>
2024-03-12 13:04:36 -0700

    Update 01_lastools_preCloudCompare_projectNAD83_clip_GeneralWorkflow.bat

--- L1/L1_Preprocessing/01_lastools_preCloudCompare_projectNAD83_clip_GeneralWorkflow.bat
+++ L1/L1_Preprocessing/01_lastools_preCloudCompare_projectNAD83_clip_GeneralWorkflow.bat
@@ -8,11 +8,14 @@ REM (4) Saves clipped DAP clouds as laz files to save storage space
 
 REM explanations for path placeholders
 REM path_to_DAP_laz_file -> path without "" and with \ between folders to the DAP laz files
-REM output_path_to_store_the_projected_DAP_laz_file -> path to location to write the projected DAP laz
+REM path_to_write -> path to location to write the projected DAP laz
+
+REM pause below is functioning as a safety, in case this .bat file is double clicked by mistake and runs, the pause will allow the chance to cancel it before it overwrites data
+pause
 
 REM General workflow 
-las2las -i path_to_DAP_laz_file ^
-	-odir output_path_to_store_the_projected_DAP_laz_file ^
+las2las -i path_to_DAP_las_file ^
+	-odir path_to_write ^
 	-odix _nad83 ^
 	-olaz ^
 	-nad83 ^
@@ -20,10 +23,10 @@ las2las -i path_to_DAP_laz_file ^
 	-cpu64 ^
 	-v
 REM projecting L1 Lidar to NAD 83
-REM path_to_L1_las -> path to the L1 .las file
-REM path_to_write_projected_L1_to -> where the projected L1 will be written to 
+REM path_to_L1_las -> path to the L1 .las file from DJI terra
+REM path_to_write -> where the projected L1 will be written to 
 las2las -i path_to_L1_las ^
-	-odir path_to_write_projected_L1_to ^
+	-odir path_to_write ^
 	-odix _nad83 ^
 	-olaz ^
 	-nad83 ^
@@ -31,9 +34,10 @@ las2las -i path_to_L1_las ^
 	-cpu64 ^
 	-v
 REM dropping points above 160m (noise points, 160m was the value for this dataset)
-REM suggestion: plot a histogram of Z values to see where the upper noise is to set this threshold should be, you might also not need to do this step if you dont have upper or lower noise
-las2las -i O:\PARSER_Ext\Rainbow_L1\2023_06_22_v39\lidars\terra_las\projected_to_NAD83\cloud_merged_nad83.laz ^
-	-odir O:\PARSER_Ext\Rainbow_L1\2023_06_22_v39\lidars\terra_las\projected_to_NAD83 ^
+REM suggestion: plot a histogram of Z values (in R) to see where the upper noise is to set this threshold, you might also not need to do this step if you dont have upper or lower noise
+REM : path_to_projected_L1 -> path to projected .laz file from above
+las2las -i path_to_projected_L1 ^
+	-odir path_to_write ^
 	-odix _droppedPtsAbove160m ^
 	-olaz ^
 	-drop_z_above 160 ^
@@ -41,61 +45,16 @@ las2las -i O:\PARSER_Ext\Rainbow_L1\2023_06_22_v39\lidars\terra_las\projected_to
 	-v
 
 pause
-REM clipping RA lidar to shp, used larger RA shp for 2022_11_20
+
+REM clipping lidar to shp
 REM this step is done to decrease the storage size so that loading into cloud compare goes smoothly and the alignment process does not crash
-set RA_poly = O:\PARSER_Ext\Rainbow_L1\PlotA\Rainbow_A_shp_nad83\Larger_shp\Rainbow_PlotA_large.shp
-lasclip -i O:\PARSER_Ext\Rainbow_L1\2023_06_22_v39\lidars\terra_las\projected_to_NAD83\cloud_merged_nad83_droppedPtsAbove160m.laz -merged -poly O:\PARSER_Ext\Rainbow_L1\PlotA\Rainbow_A_shp_nad83\Larger_shp\Rainbow_PlotA_large.shp -odir O:\PARSER_Ext\Rainbow_L1\2023_06_22_v39\lidars\terra_las\projected_to_NAD83\PlotA\ -odix _plotA -olaz
+
+REM path_to_projected_below160m_L1 <- path to .laz file that has been clipped to the site shp and filtered for points above a set threshold
+REM path_to_shp -> path to the site shapefile
+REM "-odix _plotA" will save the new laz file with the same name as the input file with "_plotA" attached to the end, change the "_plotA" to work with your naming system
+lasclip -i path_to_projected_below160m_L1 -merged -path_to_shp -odir path_to_write -odix _plotA -olaz
 	
-
-REM clipping RB lidar to shp
-REM DO PLOT B FOR 2022_11_20_v393
-set RB_poly = O:\PARSER_Ext\Rainbow_L1\PlotB\Rainbow_B_shp_nad83\Rainbow_B_Project.shp
-lasclip -i O:\PARSER_Ext\Rainbow_L1\2023_06_22_v39\lidars\terra_las\projected_to_NAD83\cloud_merged_nad83_droppedPtsAbove160m.laz -merged -poly O:\PARSER_Ext\Rainbow_L1\PlotB\Rainbow_B_shp_nad83\Rainbow_B_Project.shp -odir O:\PARSER_Ext\Rainbow_L1\2023_06_22_v39\lidars\terra_las\projected_to_NAD83\PlotB\ -odix _plotB -olaz
-
-
-REM reading in clipped DAP cloud and saving from a las to a laz for CC
-REM site: RA
-las2las -i L:\Cw_PR_Rainbow_SiteA\Flights\2022_06_07\2_Inputs\metashape\1_TILES\RGB_DenseCloud\Clipped\RGB_DenseCloud_PlotA.las ^
-	-odir L:\Cw_PR_Rainbow_SiteA\Flights\2022_06_07\2_Inputs\metashape\1_TILES\RGB_DenseCloud\Clipped\ ^
-	-olaz ^
-	-cpu64 ^
-	-v
-
-REM site: RB
-las2las -i L:\Cw_PR_Rainbow_SiteB\Flights\2022_06_07\2_Inputs\metashape\1_TILES\DenseCloud_filtered\Clipped\DenseCloud_NoNormals_NAD83_offset.las ^
-	-odir L:\Cw_PR_Rainbow_SiteB\Flights\2022_06_07\2_Inputs\metashape\1_TILES\DenseCloud_filtered\Clipped\ ^
-	-olaz ^
-	-cpu64 ^
-	-v
-
-
-REM site: CANOE -----------------------------------------------------------------------------------------------
-REM projecting Lidar to NAD 83 for 2022_04_23_v39
-las2las -i O:\PARSER_Ext\Canoe_L1\2022_04_23_v39\lidars\terra_las\cloud_merged.las ^
-	-odir O:\PARSER_Ext\Canoe_L1\2022_04_23_v39\lidars\terra_las\projected_to_NAD83 ^
-	-odix _nad83 ^
-	-olaz ^
-	-nad83 ^
-	-utm 10north ^
-	-cpu64 ^
-	-v
-
-REM projecting Lidar to NAD 83 for 2022_11_28_v39
-las2las -i O:\PARSER_Ext\Canoe_L1\2022_11_28_v39\lidars\terra_las\cloud_merged.las ^
-	-odir O:\PARSER_Ext\Canoe_L1\2022_11_28_v39\lidars\terra_las\projected_to_NAD83 ^
-	-odix _nad83 ^
-	-olaz ^
-	-nad83 ^
-	-utm 10north ^
-	-cpu64 ^
-	-v
-
-REM projecting Lidar to NAD 83 for 2023_06_21_v39
-las2las -i O:\PARSER_Ext\Canoe_L1\2023_06_21_v39\lidars\terra_las\cloud_merged.las ^
-	-odir O:\PARSER_Ext\Canoe_L1\2023_06_21_v39\lidars\terra_las\projected_to_NAD83 ^
-	-odix _nad83 ^
-	-olaz ^
-	-nad83 ^
-	-utm 10north ^
-	-cpu64 ^
-	-v
+REM Next step: Align the L1 data to the DAP data in Cloud compare
+REM Note, convention is to align the DAP to the L1 however for this specific project it made more since to align the lidar to the DAP because:
+REM a) we had many more DAP datasets then L1
+REM b) we could not load the lidar point cloud into agisoft metashape for alignment, and had been aligning all DAP clouds to a reference DAP cloud in agisoft 
